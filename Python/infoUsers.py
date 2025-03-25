2bb5552523a785b6da87c5d1420ee1f8a3938ff8
jromeroa17 <jromeroa17@educarex.es>
2025-03-25 11:00:12 +0100

    Add files via upload

--- Python/infoUsers.py
+++ Python/infoUsers.py
@@ -4,43 +4,46 @@
 #Fecha:12-03-2025
 import os
 import subprocess
+import cpuinfo
 
-def get_user():
-    files = open('/etc/passwd', 'r')
-
-    passwd = files.readlines()
-
-    lines = []
-    for user in passwd:
-        line = user.split(":")
-        lines.append(line)
-
-    userNames = []
-    for line in lines:
-        userNames.append(line[0])
-
-    name = input("Dime un usuario: ")
-
+def get_user(name):
     if name in userNames:
-        print("Usuario Econtrado")
+        print("Usuario Encontrado")
         ind = userNames.index(name)
         print(passwd[ind])
     else:
         subprocess.run(['sudo','useradd',f'{name}'])
         subprocess.run(['tail','-n','1','/etc/passwd'])
 
-def get_dir():
-    ndirec=input("Dime un directorio: ")
+#Hay que optimizarlo pero al menos no saca líneas repetidas (Busca solo los nombres de los usuarios, no que haya algún root por ahí suelto)
+#Me pasa por hacerlo dos semanas antes
 
+def get_dir(ndirec):
     if os.path.isdir(ndirec) and os.path.exists(ndirec):
         print("Es un directorio")
 
-    else:
-        print("No es un directorio")
+    elif not os.path.exists(ndirec):
+        print("No existe")
         print(f"Creando {ndirec}")
         os.makedirs(ndirec)
+    else:
+        print("Es un fichero")
 
+# Sacando los nombres de usuario de /etc/passwd
+files = open('/etc/passwd', 'r')
 
+passwd = files.readlines()
+
+lines = []
+for user in passwd:
+    line = user.split(":")
+    lines.append(line)
+
+userNames = []
+for line in lines:
+    userNames.append(line[0])
+
+# El menú
 if os.getuid() == 0:
     print("Esto es un menú")
     print("Opción 1- Mostrar información SSOO y CPU")
@@ -53,10 +56,13 @@ if os.getuid() == 0:
             case 1:
                 print(f'Tu sistema operativo es {os.uname().sysname}')
                 print(f'Tu CPU tiene una arquitectura {os.uname().machine}')
+                print(cpuinfo.get_cpu_info()['brand_raw'])
             case 2:
-                get_user()
+                name = input("Dime un usuario: ")
+                get_user(name)
             case 3:
-                get_dir()
+                ndirec = input("Dime un directorio: ")
+                get_dir(ndirec)
             case 4:
                 print("Saliendo del programa")
                 break
